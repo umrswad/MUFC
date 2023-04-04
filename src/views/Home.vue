e60af81008948d99c8c39a5eba9846fb371de299
umrswad <2250652265@qq.com>
2023-04-04 21:34:48 +0800

    首页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="home">
-    光荣属于曼联
+    <div class="logo"></div>
+    <div class="title">
+      <div class="top">MUGG</div>
+      <div class="bottom">光荣属于曼联</div>
+    </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 export default defineComponent({
-  name: 'Home'
+  name: "Home",
 });
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
-module.exports = {
-    lintOnSave: false
+const path = require("path");
+function resolve(dir) {
+  return path.join(__dirname, dir);
 }
+module.exports = {
+  lintOnSave: false,
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set("@", resolve("src"))
+      .set("@assets", resolve("src/assets"))
+      .set("@components", resolve("src/components"))
+      .set("@base", resolve("baseConfig"))
+      .set("@public", resolve("public"));
+  },
+};
